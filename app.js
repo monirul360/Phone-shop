7e57a6d7405fac1a74eeae34b811960f6a1e24ab
monirul121 <monirulislam65361@gmail.com>
2022-03-01 15:18:51 +0600

    displayShow function add

--- app.js
+++ app.js
@@ -9,5 +9,22 @@ const searchButton = () => {
 }
 
 const displayShow = show => {
-
+    const searchResult = document.getElementById("search-result");
+    searchResult.textContent = "";
+    console.log(show);
+    for (result of show.slice(0, 20)) {
+        const create = document.createElement("div");
+        create.classList.add("col-lg-4");
+        create.innerHTML = `
+        <div class="p-3 m-2 bg-white rounded shadow-lg">
+                <h3>${result.brand}</h1>
+                 <img class="img-fluid d-block mx-auto" src="${result.image}" alt="">
+                 <p class="mb-2 mt-2">
+                    Model number : ${result.phone_name}
+                  </p>
+                  <button class="btn btn-danger px-5">Details</button>
+             </div>
+        `
+        searchResult.appendChild(create);
+    }
 }
