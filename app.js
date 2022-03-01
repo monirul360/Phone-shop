4ecf3deaf81cc9d55f317b6572ef1d4fc9c4e04d
monirul121 <monirulislam65361@gmail.com>
2022-03-01 14:57:47 +0600

    main section add

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const searchButton = () => {
-    console.log("click button");
+    const searchField = document.getElementById("input-field");
+    const searchText = searchField.value;
+    searchField.value = "";
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    fetch(url)
+        .then(Response => Response.json())
+        .then(show => displayShow(show.data));
+}
+
+const displayShow = show => {
+
 }
