5b4beec3b6112e767247b8534d5ed4ea49517c7f
monirul121 <monirulislam65361@gmail.com>
2022-03-02 18:49:53 +0600

    responsive card

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const displayShow = show => {
         found.textContent = "";
         for (result of show.slice(0, 20)) {
             const create = document.createElement("div");
-            create.classList.add("col-lg-4");
+            create.classList.add("col-md-4");
             create.innerHTML = `
         <div class="p-3 m-2 bg-white rounded shadow-lg">
                  <img class="img-fluid d-block mx-auto" src="${result.image}" alt="">
